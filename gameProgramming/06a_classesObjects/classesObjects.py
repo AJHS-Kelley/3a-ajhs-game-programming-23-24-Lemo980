98511097e30fcbda49a2cbee9b8fd965b545048b
EliotB <s11806270@students.duvalschools.org>
2023-12-11 11:47:31 -0500

    Update classesObjects.py

--- gameProgramming/06a_classesObjects/classesObjects.py
+++ gameProgramming/06a_classesObjects/classesObjects.py
@@ -10,3 +10,13 @@ class Person: #Ckass names should be PascalCase
         self.name = name
         self.weight = weight
         self.birthday = birthday
+    #__str__ method Format
+    def __str__(self):
+        return f"{self.name} is {self.age} years old. \n They have {self.hairColor} hair and weigh {self.weight} pounts. \n They were born on {self.birthday} and stands {self.height} tall."
+
+#A class is a blueprint to make an object
+examplePerson1 = Person(37,"4'8\"", "black", "Gerald", 45, "April 03")
+examplePerson2 = Person(43,"10'11\"", "red", "Harold", 863, "March 25")
+print(examplePerson1)
+print(examplePerson2)
+
