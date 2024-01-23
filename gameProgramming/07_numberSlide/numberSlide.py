e9f107ff5c197b6ad5a9417228196985f9d20cd4
EliotB <s11806270@students.duvalschools.org>
2024-01-23 11:45:34 -0500

    Create numberSlide.py

---
+++ gameProgramming/07_numberSlide/numberSlide.py
@@ -0,0 +1,24 @@
+# Number slide game, ELiot Blanton, based on project by Al Sweigart, v0.0
+
+#GAME SETUP/STEPS:
+    #layout/setup grid
+    #create tiles and add to the board
+    #check for valid movements
+    #animate tile movement
+    #allow for user input to select tile (mouse click)
+    #identify location coordinates of mouse click to determine tile selected
+    #check for win -- all numbers in order
+
+#Divide and conquer method -- break down the problem into smaller problems, then solve those problems
+
+
+
+
+
+
+
+
+
+
+
+
