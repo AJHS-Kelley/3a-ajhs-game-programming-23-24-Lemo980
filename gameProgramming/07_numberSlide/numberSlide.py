1aa3909fe495454737cc62d32503ac15662b6b2c
EliotB <s11806270@students.duvalschools.org>
2024-01-23 12:00:08 -0500

    Update numberSlide.py

--- gameProgramming/07_numberSlide/numberSlide.py
+++ gameProgramming/07_numberSlide/numberSlide.py
@@ -22,9 +22,29 @@ TILESIZE = 80 #measured in pixels
 WINDOWWIDTH = 640 #measured in pixels
 WINDOWHEIGHT = 480 #measured in pixels
 
+#frames per second
+FPS = 30 #sets maximum, does not improve performance on a potato
+
+#blank tile value
+BLANK = None
 
 
+#color values
+#min value -- 0           max value -- 255
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BRIGHTBLUE = (0, 50, 255)
+DARKTURQUOISE = (3, 54, 73)
+GREEN = (0, 204, 0)
 
+#assign colors to game objects
+BGCOLOR = DARKTURQUOISE
+TILECOLOR = GREEN
+TEXTCOLOR = WHITE
+BORDERCOLOE = BRIGHTBLUE
+
+#Font size
+BASICFONTSIZE = 20 #Measured in pixels
 
 
 
