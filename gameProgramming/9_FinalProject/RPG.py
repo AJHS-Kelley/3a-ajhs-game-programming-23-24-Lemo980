577848b12e283adaf282234ce3db5c52f21dc14d
EliotB <111383052+Lemo980@users.noreply.github.com>
2024-04-02 13:21:16 -0400

    v0.0

---
+++ gameProgramming/9_FinalProject/RPG.py
@@ -0,0 +1,17 @@
+#Dungeon crawler by Eliot Blanton, v0.0
+
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((1024, 786))
+pygame.display.set_caption('Dungeon crawler')
+clock = pygame.time.Clock()
+game_active = True
+startTime = 0
+
+
+
+
+
+
