9251d0618ce1f9e4c5cf77e47a861d9d55691ee6
EliotB <111383052+Lemo980@users.noreply.github.com>
2024-04-10 13:20:23 -0400

    v0.1
    v0.1

--- gameProgramming/9_FinalProject/RPG.py
+++ gameProgramming/9_FinalProject/RPG.py
@@ -10,8 +10,18 @@ clock = pygame.time.Clock()
 game_active = True
 startTime = 0
 
+titleScreen = pygame.image.load('gameProgramming\9_FinalProject\graphics\dungeonBackground.png').convert()
 
+jungleButton = pygame.image.load('gameProgramming\9_FinalProject\graphics\JungleButton.png').convert()
+dungeonButton = pygame.image.load('gameProgramming\9_FinalProject\graphics\DungeonButton.png').convert()
+jButtonRect = jungleButton.get_rect()
+dButtonRect = dungeonButton.get_rect()
 
+while game_active:
+    screen.blit(titleScreen, (0,0))
+    screen.blit(dungeonButton, (800, 200))
+    screen.blit(jungleButton, (100, 200))
+    pygame.display.update()
 
 
 
