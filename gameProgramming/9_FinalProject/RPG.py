07ed8fedfbf82e86f2a7b7346cd0c87d22fd053c
EliotB <111383052+Lemo980@users.noreply.github.com>
2024-05-02 13:11:17 -0400

    v1.5
    v1.5

--- gameProgramming/9_FinalProject/RPG.py
+++ gameProgramming/9_FinalProject/RPG.py
@@ -9,17 +9,17 @@ pygame.display.set_caption('Dungeon crawler')
 clock = pygame.time.Clock()
 game_active = True
 startTime = 0
-pixelFont = pygame.font.Font('9_FinalProject\graphics\_fonts\Pixeltype.ttf', 100)
+pixelFont = pygame.font.Font('gameProgramming\9_FinalProject\graphics\_fonts\Pixeltype.ttf', 85)
 # create a surface to draw the font. use .render() to put font on surface 
-titleScreen = pygame.image.load('9_FinalProject\graphics\TitleBG.png').convert()
+titleScreen = pygame.image.load('gameProgramming\9_FinalProject\graphics\TitleBG.png').convert()
 titleScreenRect = titleScreen.get_rect(center = (512, 393))
 
 def selectMap():
     # selectLvL():
     mapChosen = False
     Map = ""
-    jungleButton = pygame.image.load('9_FinalProject\graphics\JungleButton.png').convert()
-    dungeonButton = pygame.image.load('9_FinalProject\graphics\DungeonButton.png').convert()
+    jungleButton = pygame.image.load('gameProgramming\9_FinalProject\graphics\JungleButton.png').convert()
+    dungeonButton = pygame.image.load('gameProgramming\9_FinalProject\graphics\DungeonButton.png').convert()
     jButtonRect = jungleButton.get_rect(center = (850, 600))
     dButtonRect = dungeonButton.get_rect(center = (150, 600))
     while mapChosen == False:
@@ -47,8 +47,8 @@ def selectMap():
     return Map
 
 def selectDiff():
-    easyButton = pygame.image.load('9_FinalProject\graphics\easyButton.png').convert()
-    hardButton = pygame.image.load('9_FinalProject\graphics\hardButton.png').convert()
+    easyButton = pygame.image.load('gameProgramming\9_FinalProject\graphics\easyButton.png').convert()
+    hardButton = pygame.image.load('gameProgramming\9_FinalProject\graphics\hardButton.png').convert()
     easyButtonRect = easyButton.get_rect(center = (890, 600))
     hardButtonRect = hardButton.get_rect(center = (130, 600))
     chooseDiffTxt = pixelFont.render('Choose Your Difficulty', False, (0, 0, 0))
@@ -85,6 +85,18 @@ def selectDiff():
 Map = selectMap()
 difficulty = selectDiff()
 
+if difficulty == "easy":
+    coins = 200
+else:
+    coins = 100
 
 def shop(coins):
-    pass
+    coinCount = pixelFont.render(str(coins)+" coins left", False, (200, 200, 0))
+    coinCountRect = coinCount.get_rect(midleft = (20, 50))
+    while True:
+        shopBG = pygame.image.load('gameProgramming\9_FinalProject\graphics\shopBG.png').convert()
+        screen.blit(shopBG, (0,0))
+        screen.blit(coinCount, coinCountRect)
+        pygame.display.update()
+
+shop(coins)
